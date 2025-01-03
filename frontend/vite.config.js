b879c995a6e240e62c5fe7543ff1cee539e069b9
Juan Gervatovski <juangervatovski@MacBook-Air-de-Juan.local>
2025-01-03 01:29:07 -0300

    Fix backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist', // Directorio de salida para producción
     rollupOptions: {
       external: ['framer-motion']['react-icons'] // Asegura que framer-motion se trate como dependencia externa
     }
