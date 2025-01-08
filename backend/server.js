a0036356064cf0f3af99584e2b74287350c9047d
Juan Gervatovski <juangervatovski@MacBook-Air-de-Juan.local>
2025-01-08 16:35:12 -0300

    Corrección

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,15 @@ app.use(cors({
 
 // Configuración de transporte para envío de correos
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,  // true para 465, false para otros puertos
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
