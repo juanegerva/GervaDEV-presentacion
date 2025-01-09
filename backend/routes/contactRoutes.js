20b95aaba5a4b49c943c382aa7be9ac8093f098e
Juan Gervatovski <juangervatovski@MacBook-Air-de-Juan.local>
2025-01-08 23:57:32 -0300

    Corrección

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
-  secure: true,
+  secure: true,  // true para puerto 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
